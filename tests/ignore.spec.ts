5fe03b4bf994fbe8bbce4ca8ad8f76230c39f71a
Mohammed Alamri <modev32@gmail.com>
2022-12-01 06:12:07 +0300

    Better tests for `ignore-attribute`

--- tests/ignore.spec.ts
+++ tests/ignore.spec.ts
@@ -1,7 +1,7 @@
 import test, { expect } from "@playwright/test";
 
 test("suf-ignore", async ({ page }) => {
-  await page.goto("test/ignore");
-  await expect(page.getByText("Email registered")).toBeVisible();
-  await expect(page.getByText("Search registered")).not.toBeVisible();
+  await page.goto("examples/ignore-attribute");
+  const registeredList = page.getByTestId('registered');
+  await expect(registeredList).toHaveText('emailname'); // as emailname is the list content: - email - name
 });
